f413c883c7e32a2b64341f845664086d49b97ab2
YangShuxuan <yangshuxuan@adups.com>
2017-03-07 15:44:55 +0800

    增加了保存imei去重后的表存储，可费劲了 重写了save功能，使得代码更加简洁了 增加了日志功能 增加了结果比较功能 增加了邮件发送功能

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "0.9.6"
 organization := "adups"
 
 libraryDependencies += "com.databricks" %% "spark-avro" % "2.0.1"
+
+libraryDependencies += "org.clapper" %% "grizzled-slf4j" % "1.3.0"
--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -2,7 +2,7 @@ package wuxi99
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
-import DeviceInfoDataFrame.{computeFotaSalesCount}
+import DeviceInfoDataFrame.{computeFotaSalesCount,compareWithWXJ}
 object CtFotaSalesApp {
   def main(args: Array[String]) {
     if (args.length != 1) {
@@ -24,6 +24,7 @@ object CtFotaSalesApp {
     implicit val sqlHiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
     val Array(pt) = args
     computeFotaSalesCount(pt)
+    compareWithWXJ(pt)
     sc.stop()
   }
 }
